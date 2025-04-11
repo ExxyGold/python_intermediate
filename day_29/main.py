8f7c55ba94ea0bd75e3f8d4506354bebf8aafba1
Exaltgod Mokonogho <mokonoghoexalt@gmail.com>
2025-04-11 13:03:42 +0100

    password upgrade

--- day_29/main.py
+++ day_29/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -22,21 +23,53 @@ def generate_password():
     pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def find_password():
+    website= website_entry.get()
+    try:
+        with open("./day_29/data.json", mode = "r") as up_file:
+            data_finder = json.load(up_file)
+    except:
+        messagebox.showerror(title= "Error", message = 'No data File found')
+    else:
+        try:
+            messagebox.showinfo(title= website, message = f"Email: {data_finder[website]["email"]}\nPassword: {data_finder[website]["password"]}")
+        except: 
+            messagebox.showerror(title= "Error", message = 'No details for the website exists')
+
 def save():
     email = email_entry.get()
     website = website_entry.get()
     password = password_entry.get()
+    new_data = { 
+         website: {
+              "email": email, 
+              "password": password
+                           }
+         
+    }
+
+
+
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title= "Oops", message= "Please dont leave any of the field empty")
     else:
-        is_ok = messagebox.askokcancel(title = website , message= f"These are the details entered:\nEmail: {email}\nPassword: {password}\nIs it ok to save?")
-        
-        if is_ok:
-            with open("./day_29/data.txt", mode = "a") as file:
-                file.write(f"{website} | {email} | {password} \n")
-
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+            try:
+                with open("./day_29/data.json", mode = "r") as file:
+                 
+                    data = json.load(file)
+            except:
+                with open("./day_29/data.json", mode = "w") as file:
+                    json.dump(new_data, file, indent = 4)
+                
+            else:
+                data.update(new_data)
+                
+                with open("./day_29/data.json", mode = "w") as up_file:
+                    json.dump(data, up_file, indent = 4)
+                    
+            finally:
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -60,8 +93,8 @@ email_label.grid(column = 0, row = 2)
 password_label = Label(text = "Password:")
 password_label.grid(column = 0, row = 3)
 
-website_entry = Entry(width=41)
-website_entry.grid(column = 1, row = 1, columnspan= 2)
+website_entry = Entry(width=21)
+website_entry.grid(column = 1, row = 1)
 website_entry.focus()
 
 email_entry = Entry(width = 41)
@@ -77,5 +110,9 @@ gen_button.grid(column = 2, row = 3)
 
 add_button = Button(text = "Add", width= 36, command = save)
 add_button.grid(column = 1, row = 4, columnspan= 2)
+
+search_button = Button(text = "Search", width = 15, command= find_password)
+search_button.grid(column = 2, row = 1)
+
 
 window.mainloop()
