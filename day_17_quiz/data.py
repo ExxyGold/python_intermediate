d141b90c7278cfff162fde9b3ba9d3400aeb301d
Exaltgod Mokonogho <mokonoghoexalt@gmail.com>
2025-04-24 18:13:46 +0100

    weather

--- day_17_quiz/data.py
+++ day_17_quiz/data.py
@@ -2,84 +2,84 @@ question_data = {
     "response_code": 0,
     "results": [
         {
-            "type": "boolean",
-            "difficulty": "medium",
-            "category": "Science: Computers",
-            "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
-            "correct_answer": "False",
-            "incorrect_answers": ["True"]
+"type": "boolean",
+"difficulty": "medium",
+"category": "Science: Computers",
+"question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+"correct_answer": "False",
+"incorrect_answers": ["True"]
         },
         {
-            "type": "boolean",
-            "difficulty": "easy",
-            "category": "Science: Computers",
-            "question": "Linux was first created as an alternative to Windows XP.",
-            "correct_answer": "False",
-            "incorrect_answers": ["True"]
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Computers",
+"question": "Linux was first created as an alternative to Windows XP.",
+"correct_answer": "False",
+"incorrect_answers": ["True"]
         },
         {
-            "type": "boolean",
-            "difficulty": "medium",
-            "category": "Science: Computers",
-            "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
-            "correct_answer": "True",
-            "incorrect_answers": ["False"]
+"type": "boolean",
+"difficulty": "medium",
+"category": "Science: Computers",
+"question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+"correct_answer": "True",
+"incorrect_answers": ["False"]
         },
         {
-            "type": "boolean",
-            "difficulty": "easy",
-            "category": "Science: Computers",
-            "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
-            "correct_answer": "True",
-            "incorrect_answers": ["False"]
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Computers",
+"question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+"correct_answer": "True",
+"incorrect_answers": ["False"]
         },
         {
-            "type": "boolean",
-            "difficulty": "easy",
-            "category": "Science: Computers",
-            "question": "The Windows ME operating system was released in the year 2000.",
-            "correct_answer": "True",
-            "incorrect_answers": ["False"]
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Computers",
+"question": "The Windows ME operating system was released in the year 2000.",
+"correct_answer": "True",
+"incorrect_answers": ["False"]
         },
         {
-            "type": "boolean",
-            "difficulty": "medium",
-            "category": "Science: Computers",
-            "question": "The &#039;Raspberry Pi&#039; series of single-board computers were created in the United States.",
-            "correct_answer": "False",
-            "incorrect_answers": ["True"]
+"type": "boolean",
+"difficulty": "medium",
+"category": "Science: Computers",
+"question": "The &#039;Raspberry Pi&#039; series of single-board computers were created in the United States.",
+"correct_answer": "False",
+"incorrect_answers": ["True"]
         },
         {
-            "type": "boolean",
-            "difficulty": "easy",
-            "category": "Science: Computers",
-            "question": "The Windows 7 operating system has six main editions.",
-            "correct_answer": "True",
-            "incorrect_answers": ["False"]
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Computers",
+"question": "The Windows 7 operating system has six main editions.",
+"correct_answer": "True",
+"incorrect_answers": ["False"]
         },
         {
-            "type": "boolean",
-            "difficulty": "medium",
-            "category": "Science: Computers",
-            "question": "&quot;Windows NT&quot; is a monolithic kernel.",
-            "correct_answer": "False",
-            "incorrect_answers": ["True"]
+"type": "boolean",
+"difficulty": "medium",
+"category": "Science: Computers",
+"question": "&quot;Windows NT&quot; is a monolithic kernel.",
+"correct_answer": "False",
+"incorrect_answers": ["True"]
         },
         {
-            "type": "boolean",
-            "difficulty": "easy",
-            "category": "Science: Computers",
-            "question": "Ada Lovelace is often considered the first computer programmer.",
-            "correct_answer": "True",
-            "incorrect_answers": ["False"]
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Computers",
+"question": "Ada Lovelace is often considered the first computer programmer.",
+"correct_answer": "True",
+"incorrect_answers": ["False"]
         },
         {
-            "type": "boolean",
-            "difficulty": "medium",
-            "category": "Science: Computers",
-            "question": "The first computer bug was formed by faulty wires.",
-            "correct_answer": "False",
-            "incorrect_answers": ["True"]
+"type": "boolean",
+"difficulty": "medium",
+"category": "Science: Computers",
+"question": "The first computer bug was formed by faulty wires.",
+"correct_answer": "False",
+"incorrect_answers": ["True"]
         }
     ]
 }
---
+++ day_35/main.py
@@ -0,0 +1,52 @@
+import os
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+auth_token = os.getenv("AUTH_TOKEN")
+
+api_key = "d45b6a63b43af269fb9d12267329dd5b"
+
+account_sid = os.getenv("ACCOUNT_SID")
+
+
+
+My_lat = 5.89
+my_long = 5.67
+
+parameter = {"lat": 5.89,
+    "lon": 5.67,
+    "cnt":4,
+    "appid": api_key
+}
+
+
+weather = requests.get(url = "https://api.openweathermap.org/data/2.5/forecast", params= parameter)
+weather.raise_for_status()
+
+print(weather.status_code)
+weather_data = weather.json()
+
+data_list = weather_data["list"]
+
+will_rain = False
+
+for time in data_list:
+
+    each_weather = time["weather"][0]
+
+    weather_id = each_weather["id"]
+    if weather_id < 700:
+        will_rain = True
+       
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+    body="Bring an umbrella cuz its going to rain☔",
+    from_="whatsapp:+14155238886",
+    to="whatsapp:+2348136552264",
+    )
+
+    print(message.status)
